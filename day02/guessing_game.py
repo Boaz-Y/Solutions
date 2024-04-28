bd4b9d1621e42e414b924fab14524428bc6d3214
Boaz-Y <34510471+Boaz-Y@users.noreply.github.com>
2024-04-28 16:19:55 +0300

    Add files via upload

--- day02/guessing_game.py
+++ day02/guessing_game.py
@@ -2,17 +2,15 @@ import random #pseudorandom library for picking of an integer
 secret_number = random.randint(1, 20) #picking a random number
 
 user_guess = int(input("Guess what the secret integer betweem 1 and 20 is ")) #The initial guess of the game, must convert variable to type integer for comparison
-
+guesses = 0
 while True: #the loop for the guesses
+    guesses += 1 #counting up the guesses, using only 1 for the whole script
     if user_guess > secret_number: #directing the player if the guess is too high
-        guesses = guesses + 1 #counting up
         print(f"Your guess is higher than the secret number. Guess again. (You've guessed {guesses} times.)") #telling the player he's wrong and to guess again
         user_guess = int(input("Guess again (a number less than " + str(user_guess) + "): ")) #asking the player for a new number
     elif user_guess < secret_number: #if the player guesses too low
-        guesses = guesses + 1 #counter
         print(f"Your guess is lower than the secret number. Guess again. (You've guessed {guesses} times.)") #notifying the player he's wrong and how many guesses
         user_guess = int(input("Guess again (a number greater than " + str(user_guess) + "): ")) #giving the player another chance to guess
     elif user_guess == secret_number: #if the player guesses correctly
-        guesses = guesses + 1 #still have to count for every guess
         print(f"Good job! You guessed the number in {guesses} guesses.")
         break #finishing the game, ending the loop
