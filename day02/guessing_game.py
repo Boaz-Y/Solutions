5e82a7eb6995ba360a92c738790b71b3139c9d26
Boaz-Y <34510471+Boaz-Y@users.noreply.github.com>
2024-05-08 13:25:07 +0300

    Update guessing_game.py

--- day02/guessing_game.py
+++ day02/guessing_game.py
@@ -7,10 +7,9 @@ while True: #the loop for the guesses
     guesses += 1 #counting up the guesses, using only 1 for the whole script
     if user_guess > secret_number: #directing the player if the guess is too high
         print(f"Your guess is higher than the secret number. Guess again. (You've guessed {guesses} times.)") #telling the player he's wrong and to guess again
-        user_guess = int(input("Guess again (a number less than " + str(user_guess) + "): ")) #asking the player for a new number
     elif user_guess < secret_number: #if the player guesses too low
         print(f"Your guess is lower than the secret number. Guess again. (You've guessed {guesses} times.)") #notifying the player he's wrong and how many guesses
-        user_guess = int(input("Guess again (a number greater than " + str(user_guess) + "): ")) #giving the player another chance to guess
     elif user_guess == secret_number: #if the player guesses correctly
         print(f"Good job! You guessed the number in {guesses} guesses.")
         break #finishing the game, ending the loop
+    user_guess = int(input("Guess again")) #asking the player for a new number
