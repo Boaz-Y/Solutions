bcce26c4d79ea33e8b56e16b3046abe5dd3fb4af
Boaz-Y <34510471+Boaz-Y@users.noreply.github.com>
2024-06-16 18:26:27 +0300

    Update fasta2csv.py

--- day06/fasta2csv.py
+++ day06/fasta2csv.py
@@ -1,11 +1,20 @@
-from Bio import SeqIO
 import csv
+import argparse
+from Bio import SeqIO
 
-fasta_file = "proteome.fasta"
-csv_file = "proteome.csv"
+def convert_fasta_to_csv(fasta_file, csv_file):
+    with open(fasta_file) as fasta, open(csv_file, "w", newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["Protein_ID", "Sequence"])
+        for record in SeqIO.parse(fasta, "fasta"):
+            csvwriter.writerow([record.id, str(record.seq)])
 
-with open(fasta_file) as fasta, open(csv_file, "w", newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["Protein_ID", "Sequence"])
-    for record in SeqIO.parse(fasta, "fasta"):
-        csvwriter.writerow([record.id, str(record.seq)])
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Convert a FASTA file to a CSV file.")
+    parser.add_argument("fasta_file", help="Path to the input FASTA file")
+    parser.add_argument("csv_file", help="Path to the output CSV file")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    convert_fasta_to_csv(args.fasta_file, args.csv_file)
